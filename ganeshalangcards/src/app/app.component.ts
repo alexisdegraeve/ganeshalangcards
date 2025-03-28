10b928ec967c1c962982a5af8a091e05db2c82d5
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-28 14:05:30 +0100

    add flashcard

--- ganeshalangcards/src/app/app.component.ts
+++ ganeshalangcards/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { FlashcardComponent } from "./flashcard/flashcard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslateModule],
+  imports: [RouterOutlet, TranslateModule, FlashcardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ ganeshalangcards/src/app/flashcard/flashcard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-flashcard',
+  imports: [TranslateModule],
+  templateUrl: './flashcard.component.html',
+  styleUrl: './flashcard.component.scss'
+})
+export class FlashcardComponent {
+
+}
