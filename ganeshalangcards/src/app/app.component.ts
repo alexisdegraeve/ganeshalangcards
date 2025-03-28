7c298f5e16e342083483777e685bf3c0a73131f8
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-28 14:10:10 +0100

    add header add footer

--- ganeshalangcards/src/app/app.component.ts
+++ ganeshalangcards/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { FlashcardComponent } from "./flashcard/flashcard.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslateModule, FlashcardComponent],
+  imports: [RouterOutlet, TranslateModule, FlashcardComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
