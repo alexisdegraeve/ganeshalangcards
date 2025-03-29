4503225be9b3aa374f7c7512794a74e20ba4382f
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-29 20:43:05 +0100

    cleaning code

--- ganeshalangcards/src/app/app.component.ts
+++ ganeshalangcards/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { FlashcardComponent } from "./flashcard/flashcard.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslateModule, FlashcardComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, TranslateModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
