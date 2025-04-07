8a5ba14aef3caf4d7dc8539c5a386583f882b5ab
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-04-07 16:31:35 +0200

    change header

--- ganeshalangcards/src/app/header/header.component.ts
+++ ganeshalangcards/src/app/header/header.component.ts
@@ -1,14 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [TranslateModule, RouterModule],
+  imports: [TranslateModule, RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent  {
+  defaultLang: string = 'en';
+
   constructor(private translate: TranslateService) {
     translate.addLangs(['en', 'fr']);
     translate.setDefaultLang('en');
@@ -16,9 +19,11 @@ export class HeaderComponent {
     // Détecte la langue du navigateur
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang?.match(/en|fr/) ? browserLang : 'en');
+    translate.use(this.defaultLang);
   }
 
   changeLanguage(lang: string) {
+    this.defaultLang = lang;
     this.translate.use(lang);
   }
 
